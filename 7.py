f50896979917d7a49b550e97c6403e77e95a846c
Jennifer Hamon <jhamon@gmail.com>
2013-01-31 00:56:39 -0800

    My first version was slow (~1m) because I was checking too many values, but after reading about Sieve of Eratosthenes was able to speed up to <1s.

---
+++ 7.py
@@ -0,0 +1,15 @@
+from math import sqrt
+# The 10001st prime number
+
+primelist = [2,3]
+i = 5
+while len(primelist)<10001:
+    upperbound = int(sqrt(i))+1 # Only check to sqrt(n)
+    for j in range(2,upperbound+1):
+        if i%j==0: # Divisible, so not prime
+            break 
+        if j == upperbound:
+            primelist.append(i)
+            break
+    i = i + 2 # increment by 2, to check odds only
+print primelist[-1] # Answer!
