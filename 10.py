83099b99d358a5f9dcf2e88e53bff630198e2514
Jennifer Hamon <jhamon@gmail.com>
2013-02-18 18:14:55 -0800

    Modified: 10.py.  Major refactor with 400% speed improvement
    Changing to list comprehensions and not checking evens for primeness
    really sped things up a lot.

--- 10.py
+++ 10.py
@@ -1,17 +1,15 @@
-# Sum of all primes less than 2 million
+# Find the sum of all primes smaller than 2 million
+
 from math import sqrt
 
-primelist = [2, 3]
-i = 5
-while primelist[-1] < 2000000:
-    upperbound = int(sqrt(i)) + 1  # Only check to sqrt(n)
-    for j in xrange(2, upperbound + 1):
-        if i % j == 0:  # Divisible, so not prime
-            break
-        if j == upperbound:
-            primelist.append(i)
-            break
-    i = i + 2  # increment by 2, to check odds only
+def is_prime(n):
+	factors_to_check = xrange(3,int(sqrt(n))+1,2)
+	for fac in factors_to_check:
+		if not n % fac:
+			return False
+	else:
+		return True
 
-
-print 'sum: ', sum(primelist[:-1])
+primelist = [i for i in xrange(3,2000000,2) if is_prime(i)]
+primelist.insert(0,2) # 2 is the smallest prime
+print sum(primelist) # Answer: 142913828922
