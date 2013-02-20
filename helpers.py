ced1a2399f1df127335db9812db80b3299255bbc
Jennifer Hamon <jhamon@gmail.com>
2013-02-20 11:14:39 -0800

    Modified: helpers.py to add a prime generator

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from math import sqrt
+from itertools import count
 
 def is_prime(x):
 	""" Slow method for determining whether a number is prime
@@ -20,6 +21,11 @@ def is_prime(x):
 		return True 
 
 
+def divisible(a, b):
+    """Returns true if a divisible by b"""
+    return not (a % b)
+
+
 def gcd(x, y):
 	""" returns greatest common divisor """
 	if x < y:
@@ -31,5 +37,20 @@ def gcd(x, y):
 
 def lcm(x, y):
 	""" returns the least common multiple """
-
 	return x * y / gcd(x, y)
+
+odd_gen = count(3, 2)
+def prime_generator():
+    """ Generator function emits one prime at a time"""
+    genprimelist = [2]
+    def _is_prime(n):
+        factors = [n % x for x in genprimelist if x <= sqrt(n)]
+        if 0 in factors:
+        	return False
+        else:
+        	return True
+
+    for x in odd_gen:
+        if _is_prime(x):
+            genprimelist.append(x)
+            yield x
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -11,6 +11,12 @@ def test_is_prime():
 	eq_(is_prime(16), False)
 	eq_(is_prime(49), False)
 
+def test_divsible():
+	eq_(divisible(10, 5), True)
+	eq_(divisible(15, 7), False)
+	eq_(divisible(5, 10), False)
+	eq_(divisible(49, 7), True)
+
 def test_gcd():
 	eq_(gcd(5, 25), 5)
 	eq_(gcd(2, 11), 1)
@@ -25,3 +31,17 @@ def test_lcm():
 	eq_(lcm(20, 10), 20)
 	eq_(lcm(30, 14), 210)
 	eq_(lcm(3, 9), 9)
+
+def test_prime_generator():
+	myprimegen = prime_generator()
+	eq_(myprimegen.next(), 3)
+	eq_(myprimegen.next(), 5)
+	eq_(myprimegen.next(), 7)
+	eq_(myprimegen.next(), 11)
+	eq_(myprimegen.next(), 13)
+	eq_(myprimegen.next(), 17)
+	eq_(myprimegen.next(), 19)
+	eq_(myprimegen.next(), 23)
+	eq_(myprimegen.next(), 29)
+	eq_(myprimegen.next(), 31)
+	
