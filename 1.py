6050c241230f2237a0cee416a223be4d20ce6a99
Jennifer Hamon <jhamon@gmail.com>
2013-02-20 01:59:43 -0800

    Modified: 1.py and 2.py. Moved solution inside of function.

--- 1.py
+++ 1.py
@@ -1,1 +1,4 @@
-print sum([x for x in xrange(0,1000) if x%5==0 or x%3==0])
+def problem1():
+	"""Print the sum of the numbers less than 1000 which are 
+	   divisible by 3 or 5"""
+	return sum([x for x in xrange(0,1000) if x%5==0 or x%3==0])
--- 2.py
+++ 2.py
@@ -1,8 +1,8 @@
-# Sum of even fibs < 4million
+def problem2():
+	""" Returns the sum of all fibonacci numbers less than 4 million"""
+	fibs = [1, 1]
+	while fibs[-1] < 4000000:
+		fibs.append(fibs[-1]+fibs[-2])
+	fibs = fibs[:-1]
 
-fibs = [1, 1]
-while fibs[-1] < 4000000:
-	fibs.append(fibs[-1]+fibs[-2])
-fibs = fibs[:-1]
-
-print sum([x for x in fibs if x%2==0])
+	return sum([x for x in fibs if x%2==0])
