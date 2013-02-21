6acf32b675341eafce722dbd834e8eeaa21ff753
Jennifer Hamon <jhamon@gmail.com>
2013-02-21 03:04:42 -0800

    Modified: helpers.py and tests/test_helpers.py
    I moved several functions out of problems.py and into helpers.py
    in anticipation of major refactoring my problem code.

--- helpers.py
+++ helpers.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
-from math import sqrt
+from math import sqrt, log
 from itertools import count
+import types
+from operator import mul
 
 def is_prime(x):
-	""" Slow method for determining whether a number is prime
-	by checking all potential factors up to sqrt(x) until one
-	is found"""
-	if x == 1:
-		return False
-	elif x == 2:
-		return True
-	elif not x % 2: # Even number, not prime
-		return False
+    """ Slow method for determining whether a number is prime
+    by checking all potential factors up to sqrt(x) until one
+    is found"""
+    if x == 1:
+        return False
+    elif x == 2:
+        return True
+    elif not x % 2: # Even number, not prime
+        return False
 
-	for i in range(3,int(sqrt(x))+1,2):
-		if not x % i: 
-		    # Evenly divisible, not prime
-			return False
-	else:
-		return True 
+    for i in range(3,int(sqrt(x))+1,2):
+        if not x % i: 
+            # Evenly divisible, not prime
+            return False
+    else:
+        return True 
 
 
 def divisible(a, b):
@@ -27,17 +29,17 @@ def divisible(a, b):
 
 
 def gcd(x, y):
-	""" returns greatest common divisor """
-	if x < y:
-		x, y = y, x
+    """ returns greatest common divisor """
+    if x < y:
+        x, y = y, x
 
-	while y:
-		x, y = y, x % y
-	return x
+    while y:
+        x, y = y, x % y
+    return x
 
 def lcm(x, y):
-	""" returns the least common multiple """
-	return x * y / gcd(x, y)
+    """ returns the least common multiple """
+    return x * y / gcd(x, y)
 
 odd_gen = count(3, 2)
 def prime_generator():
@@ -46,11 +48,77 @@ def prime_generator():
     def _is_prime(n):
         factors = [n % x for x in genprimelist if x <= sqrt(n)]
         if 0 in factors:
-        	return False
+            return False
         else:
-        	return True
+            return True
 
     for x in odd_gen:
         if _is_prime(x):
             genprimelist.append(x)
             yield x
+
+def get_primes_less_than_p(p):
+    """ Returns a list of prime numbers smaller than p
+    Generating a list of primes for use by other functions
+    prevents having to recheck for primeness."""
+    primelist = [i for i in xrange(3, p, 2) if is_prime(i)]
+    primelist.insert(0, 2)  # 2 is the smallest prime
+    return primelist
+
+def prime_factorization_tree(n):
+    """ prime_factorization_tree(n) returns a list containing (base, exponent)
+    tuples that make up the prime factorization of n"""
+
+    if n == 1:
+        return [(1, 0)]  # This is a kludge
+
+
+    def get_factor_exponent(a, b):
+        """ For factor b of a, returns the highest
+        power of b that is divisible into a """
+        max_power_possible = int(log(a, b)) + 1
+
+        for i in xrange(max_power_possible, 0, -1):
+            if not a % (b ** i):
+                return i
+
+    prime_less_than_n = get_primes_less_than_p(n)
+
+    prime_factors = [x for x in prime_less_than_n if divisible(n,x)]
+    if prime_factors == []:
+        return [(n, 1)]
+    remainder = n
+    exponents = []
+
+    for factor in prime_factors[::-1]:  # Do big factors first
+        power = get_factor_exponent(remainder, factor)
+        exponents.insert(0, power)
+        remainder = int(remainder / (factor ** power))
+
+    return zip(prime_factors, exponents)
+
+def number_of_factors(n):
+    """ Returns the number of factors for a given integer or
+    prime factorization.  If the factorization is given, it 
+    should follow the zip(factorlist, exponentlist) structure
+    of prime_factorization_tree() output 
+
+    We need to know that a number with prime factorization
+    of the form n^a*p^b*q^c*... will have (a+1)(b+1)(c+1)*... factors.
+    """
+
+    if isinstance(n, types.ListType):
+        return reduce(mul, [x[1] + 1 for x in n])
+    elif isinstance(n, types.IntType):
+        n = prime_factorization_tree(n)
+        return reduce(mul, [x[1] + 1 for x in n])
+
+
+def tri(n):
+    """ Returns the nth triangular number"""
+    return n * (n + 1) / 2
+
+def triangle_generator():
+    """ triangular number generator """
+    for n in count(1, 1):
+        yield tri(n)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -44,4 +44,34 @@ def test_prime_generator():
 	eq_(myprimegen.next(), 23)
 	eq_(myprimegen.next(), 29)
 	eq_(myprimegen.next(), 31)
+
+def test_get_primes_less_than_q():
+	eq_(get_primes_less_than_p(10), [2, 3, 5, 7])
+	eq_(get_primes_less_than_p(20), [2, 3, 5, 7, 11, 13, 17, 19])
+	eq_(get_primes_less_than_p(23), [2, 3, 5, 7, 11, 13, 17, 19])
 	
+def test_prime_factorization_tree():
+	eq_(prime_factorization_tree(16), [(2, 4)])
+	eq_(prime_factorization_tree(100), [(2, 2), (5, 2)])
+	eq_(prime_factorization_tree(45), [(3, 2), (5, 1)])
+	eq_(prime_factorization_tree(17), [(17, 1)])
+
+def test_number_of_factors():
+	eq_(number_of_factors(25), 3)
+	eq_(number_of_factors(prime_factorization_tree(25)), 3)
+	eq_(number_of_factors(1), 1)
+	eq_(number_of_factors(2), 2)
+	eq_(number_of_factors(11), 2)
+	eq_(number_of_factors(6), 4)
+
+def test_tri():
+	eq_(tri(3), 6)
+	eq_(tri(1), 1)
+	eq_(tri(5), 15)
+
+def test_triangle_generator():
+	tri_gen = triangle_generator()
+	eq_(tri_gen.next(), 1)
+	eq_(tri_gen.next(), 3)
+	eq_(tri_gen.next(), 6)
+	eq_(tri_gen.next(), 10)			
